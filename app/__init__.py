c672442982620b6f6e6803c734e29ac072b79001
Waimea-Krclark <krclark@waimea.school.nz>
2025-05-22 12:23:55 +1200

    Created Form, 404 error and page interaction

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 from random import randint
 
 # Create the app
@@ -13,12 +14,19 @@ def home():
 def about():
     return render_template('pages/about.jinja')
 
+@app.get("/form/")
+def form():
+    return render_template('pages/form.jinja')
+
 @app.get("/random/")
 def random():
     randNum = str(randint(1,100))
     return render_template('pages/random.jinja', number = randNum)
 
-@app.route('/run_function', methods=['GET'])
-def run_function():
-    # Your Python function logic here
-    print("Python function executed!")
+@app.errorhandler(404)
+def notFound(e):
+    return render_template('pages/404.jinja')
+
+@app.get('/number/<int:num>')
+def analyseNumber(num):
+    return render_template('pages/number.jinja', number=num)
